cf3c5714cf9850efca6c0e829fc2ec8a4c079592
Luiss Montoya <luisgoemon@gmail.com>
2021-12-02 14:34:10 -0500

    Fronted end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
+import { CreateEmpleadoComponent } from './components/create-empleado/create-empleado.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListEmpleadosComponent,
+    CreateEmpleadoComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
